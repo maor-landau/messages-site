2d83b2a6b59b313708ee66a61155207caa1367c2
maor-landau <you@example.com>
2021-11-07 18:03:15 +0200

    finish the authorisation part

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,13 +1,16 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
     const isAuth = this.authService.getIsAuth();
-    return true;
+    if (!isAuth) {
+      this.router.navigate(['/login']);
+    }
+    return isAuth;
   }
 }
